d570271eb853079ecf2a0abbb1033aa8281191f4
Max Korolev <nrjshka@gmail.com>
2021-11-18 21:59:21 +0100

    Added: liquidity value calculator

---
+++ contracts/LiquidityValueCalculator.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.6;
+
+import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+
+import "./interfaces/ILiquidityValueCalculator";
+
+contract LiquidityValueCalculator is ILiquidityValueCalculator {
+  address public factory;
+
+  constructor(address factory_) public {
+    factory = factory_;
+  }
+
+  function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveB, uint totalSupply) {
+    IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+
+    totalSupply = pair.totalSupply();
+    (uint reserves0, uint reserves1, ) = pair.getReserves();
+    (reserveA, reserveB) = tokenA == pair.token0() ? (reserves0, reserves1) : (reserves1, reserves0);
+  }
+}
