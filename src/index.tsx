d6b5d7cd0976c7e61fb4d31a37a97bd0dd388bec
Max Korolev <nrjshka@gmail.com>
2021-11-20 18:03:11 +0100

    Added: prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import reportWebVitals from "./reportWebVitals"
 
-ReactDOM.render(<React.StrictMode />, document.getElementById("root"))
+import { App } from './pages'
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
